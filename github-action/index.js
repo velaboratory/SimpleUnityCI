d605d6d7f242fef5b84548233eee3f2cad674ec7
Anton Franzluebbers <anton@uga.edu>
2024-02-09 17:14:45 -0500

    do fetch with inputs

--- github-action/index.js
+++ github-action/index.js
@@ -2,14 +2,16 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+  const buildUrl = core.getInput('url');
+  const data = core.getInput('data');
+  console.log(buildUrl);
+  console.log(data);
+  const startTime = new Date();
+  console.log(startTime)
+  const r = fetch(buildUrl, { method: 'POST', body: data }).then(r => r.json()).then(r => {
+    console.log(new Date())
+    console.log(r)
+  })
 } catch (error) {
   core.setFailed(error.message);
 }
